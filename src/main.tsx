a298a7cb5c82042d76a556366672e1323f972e3c
MadhanKumarR150896 <madhankumar150896@outlook.com>
2026-06-28 18:35:01 +0530

    Setup - Supabase client and Tanstack client

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,28 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
+import { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools
+          initialIsOpen={false}
+          buttonPosition="bottom-right"
+        />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
